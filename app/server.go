23c07bc3890b9226c0a5a1484493a82c3a385ca7
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-02-06 15:50:25 +0530

    handle concurrent connections

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -11,29 +12,37 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	defaultPong := "+PONG\r\n"
+	var wg sync.WaitGroup
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			break
+		}
+		wg.Add(1)
+		go handleClient(conn, &wg)
 	}
+	wg.Wait()
+}
 
+func handleClient(conn net.Conn, wg *sync.WaitGroup) {
+	defaultPong := "+PONG\r\n"
+	defer conn.Close()
+	defer wg.Done()
 	data := make([]byte, 1024) // buffer to store incoming data
-
-    for {
-		_, err = conn.Read(data)
+	for {
+		_, err := conn.Read(data)
 		if err != nil {
 			fmt.Println("Error reading:", err.Error())
-            break
+			break
 		}
 		_, err = conn.Write([]byte(defaultPong))
 		if err != nil {
 			fmt.Println("Error sending data:", err.Error())
 		}
 	}
-
 }
