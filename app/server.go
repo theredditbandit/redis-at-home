3b3444a4df5be25378aebbffbd0427fea4cb8dd2
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-02-06 15:13:08 +0530

    reply with pong as default

--- app/server.go
+++ app/server.go
@@ -9,18 +9,31 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	data := make([]byte, 1024) // buffer to store incoming data
+
+	_, err = conn.Read(data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp := "+PONG\r\n"
+
+	_, err = conn.Write([]byte(resp))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
